4c4aee7823f87e6938e1151016b85741a36cce53
aakzsh <aakashferrari@gmail.com>
2026-05-21 10:52:06 +0530

    remove target folder

--- target/debug/deps/rage-2ff9b272a91f6ed0.d
+++
@@ -1,5 +0,0 @@
-/Users/aakash.shrivastava/projects/rage/target/debug/deps/rage-2ff9b272a91f6ed0.d: src/main.rs
-
-/Users/aakash.shrivastava/projects/rage/target/debug/deps/rage-2ff9b272a91f6ed0: src/main.rs
-
-src/main.rs:
--- target/debug/deps/rage-57aacc02eb0f4ec6.d
+++
@@ -1,5 +0,0 @@
-/Users/aakash.shrivastava/projects/rage/target/debug/deps/rage-57aacc02eb0f4ec6.d: src/main.rs
-
-/Users/aakash.shrivastava/projects/rage/target/debug/deps/librage-57aacc02eb0f4ec6.rmeta: src/main.rs
-
-src/main.rs:
